b63c9d61b4c07d415f050c54791d4d7c6fac5700
Jari Bakken <jari@holderdeord.no>
2015-01-07 00:47:15 +0100

    Add draft of laugh post

---
+++ _drafts/dette-far-politikerne-til-a-le.md
@@ -0,0 +1,35 @@
+---
+layout: post
+title: Dette får politikerne til å le
+date: "2016-12-31T23:59:59.000+01:00"
+authors:
+- Herman Westrum Thorsen
+- Anders Olsen Sandvik
+- Jari Bakken
+tags:
+- referater
+- humor
+- munterhet
+- representanter
+scripts:
+- d3
+- c3
+- __title__
+---
+
+**Politikere kan virke tørre og kjedelige, men det hender de glimter til under debattene på Stortinget. Vi har gravd i referatene for å finne ut hvem som skaper de største latterbrølene.**
+
+> Jeg mener å huske at jeg også hadde godt humør da jeg var statsminister. Jeg kan i hvert fall erindre at jeg lo flere ganger, og jeg var i alle fall tidvis engasjert.
+> <cite>Jens Stoltenberg, 22. oktober 2013</cite>
+
+Når det blir sagt noe morsomt på Stortinget, skrives det ofte inn i referatene som «munterhet i salen» eller «latter i salen», avhengig av reaksjonen fra tilhørerne.
+
+Ved å søke  i referatene kan vi skaffe oss en oversikt over de topp 10 morsomste representantene. Her ser vi antall latterutbrudd per innlegg representanten har holdt de siste seks årene:
+
+<div class="om-munterhet-representative-chart"></div>
+<figcaption>Munterhet/latter per innlegg, oktober 2008 - november 2014</figcaption>
+
+Om vi grupperer på partitilhørighet, seiler KrF - sannsynligvis takket være Hareide og Syversen - opp på første plass.
+
+<div class="om-munterhet-party-chart"></div>
+<figcaption>Munterhet/latter per innlegg, oktober 2008 - november 2014</figcaption>
---
+++ public/js/posts/dette-f-r-politikerne-til-le.js
@@ -0,0 +1,39 @@
+(function () {
+    var partyCounts = [{"name": "KrF", "count": "16", "innlegg": "5356"}, {"name": "A", "count": "17", "innlegg": "10195"}, {"name": "Sp", "count": "7", "innlegg": "4218"}, {"name": "V", "count": "5", "innlegg": "3335"}, {"name": "SV", "count": "5", "innlegg": "4784"}, {"name": "FrP", "count": "9", "innlegg": "10322"}, {"name": "H", "count": "8", "innlegg": "9688"}, {"name": "MDG", "count": "0", "innlegg": "182"}];
+    var repCounts = [{"name": "Jens Stoltenberg (A)", "count": "16", "innlegg": "907"}, {"name": "Knut Arild Hareide (KrF)", "count": "5", "innlegg": "433"}, {"name": "Torgeir Micaelsen (A)", "count": "2", "innlegg": "183"}, {"name": "Hans Olav Syversen (KrF)", "count": "6", "innlegg": "630"}, {"name": "Snorre Serigstad Valen (SV)", "count": "3", "innlegg": "318"}, {"name": "Dagfinn Høybråten (KrF)", "count": "3", "innlegg": "360"}, {"name": "Abid Q. Raja (V)", "count": "2", "innlegg": "241"}, {"name": "Per Olaf Lundteigen (Sp)", "count": "4", "innlegg": "636"}, {"name": "Jonas Gahr Støre (A)", "count": "4", "innlegg": "791"}];
+
+    function cleanCounts(data) {
+        data.forEach(function (d) {
+            d.count = +d.count;
+            d.innlegg = +d.innlegg;
+            d.score = d.count / d.innlegg;
+        });
+    }
+
+    cleanCounts(partyCounts);
+    cleanCounts(repCounts);
+
+    function drawTopList(selector, data) {
+        var cols = [];
+
+        cols.push(['x'].concat(data.map(function (d) { return d.name; })));
+        cols.push(['Score'].concat(data.map(function (d) { return d.score; })));
+
+        var chart = c3.generate({
+            bindto: selector,
+            data: {
+                x: 'x',
+                columns: cols,
+                type: 'bar'
+            },
+            axis: {
+                x: { type: 'category' },
+                rotated: true
+            },
+            color: { pattern: ["#e0c600"] }
+        });
+    }
+
+    drawTopList('.om-munterhet-representative-chart', repCounts);
+    drawTopList('.om-munterhet-party-chart', partyCounts);
+}());
