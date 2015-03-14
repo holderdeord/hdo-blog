5a4f1ef4d312565cf9f745b054c19df7a3a0f32e
Jari Bakken <jari@holderdeord.no>
2015-03-14 15:27:38 +0100

    Graph update + add video

--- _drafts/dette-er-stortingets-morsomste.md
+++ _drafts/dette-er-stortingets-morsomste.md
@@ -15,18 +15,31 @@ scripts:
   - c3
   - __title__
 published: true
+video: https://player.vimeo.com/video/122051171
+thumbnail: https://i.vimeocdn.com/video/510733790.webp?mw=960&mh=540
 ---
 
 **Selv om det er en formell tone i stortingssalen er det fremdeles rom for kjappe kommentarer og humoristiske skråblikk. Vi har gravd i referatene og Stortingets videoarkiv for å finne ut hva som får våre folkevalgte til å le.**
 
-> Jeg mener å huske at jeg også hadde godt humør da jeg var statsminister. Jeg kan i hvert fall erindre at jeg lo flere ganger, og jeg var i alle fall tidvis engasjert.
-> <cite>Jens Stoltenberg i Stortinget, 22. oktober 2013</cite>
+<iframe
+    src="{{ page.video }}"
+    width="100%"
+    height="281"
+    frameborder="0"
+    webkitallowfullscreen
+    mozallowfullscreen
+    allowfullscreen>
+</iframe>
 
 Når det blir sagt noe morsomt på Stortinget, skrives det ofte inn i referatene som «munterhet i salen» eller «latter i salen». Tidvis kan man mene at «humring i salen» vil være en mer nøyaktig beskrivelse. Humoren er preget av Stortingets tradisjonelt formelle tone. Dette er ikke et forum for «sa brura»-vitser, men heller tørvittige kommentarer av både frivillig og ufrivillig art.
 
+> Jeg mener å huske at jeg også hadde godt humør da jeg var statsminister. Jeg kan i hvert fall erindre at jeg lo flere ganger, og jeg var i alle fall tidvis engasjert.
+> <cite>Jens Stoltenberg i Stortinget, 22. oktober 2013</cite>
+
+
 Vi har tidligere søkt gjennom referatene for å finne ut [hvilke aviser politikerne leser]({% post_url 2014-12-10-hvilke-aviser-leser-politikerne %}). Ved hjelp av samme metode kan vi finne ut hvem som er de morsomste representantene.
 
-Disse fem talerne har skapt flest latterutbrudd de siste seks årene.
+Her er alle politikerne som har fått salen til å le mer enn én gang:
 
 <div class="om-munterhet-representative-chart"></div>
 <figcaption class="text-center">Antall latterutbrudd per person, oktober 2008 - februar 2015</figcaption>
@@ -40,3 +53,5 @@ Det kan virke som om stemningen er på sitt beste rett etter et valg:
 
 <div class="om-munterhet-timeline-chart"></div>
 <figcaption class="text-center">Andel innlegg med latterutbrudd per stortingssesjon, 2008 - 2014</figcaption>
+
+Du kan se alle våre bakgrunnsdata [her](https://docs.google.com/spreadsheets/d/1ibIUhKuq-h1QL_LwTYoVuKeClm59HwLSRjcyVsm6__U/edit?usp=sharing).
--- public/js/posts/dette-er-stortingets-morsomste.js
+++ public/js/posts/dette-er-stortingets-morsomste.js
@@ -1,6 +1,6 @@
 (function () {
     var colorPattern = ["#606b82", "#e1c600"];
-    var repCounts    = [{"name":"Jens Stoltenberg","party":"A","total":907,"count":18,"percent":1.9845644983461963},{"name":"Hans Olav Syversen","party":"KrF","total":664,"count":8,"percent":1.2048192771084338},{"name":"Jonas Gahr Støre","party":"A","total":808,"count":5,"percent":0.6188118811881188},{"name":"Trine Skei Grande","party":"V","total":1161,"count":5,"percent":0.4306632213608958},{"name":"Knut Arild Hareide","party":"KrF","total":449,"count":5,"percent":1.1135857461024499},{"name":"Per Olaf Lundteigen","party":"Sp","total":681,"count":4,"percent":0.5873715124816447},{"name":"Svein Flåtten","party":"H","total":491,"count":3,"percent":0.6109979633401221},{"name":"Ketil Solvik-Olsen","party":"FrP","total":990,"count":3,"percent":0.30303030303030304},{"name":"Knut Storberget","party":"A","total":794,"count":3,"percent":0.3778337531486146},{"name":"Kristin Halvorsen","party":"SV","total":940,"count":3,"percent":0.3191489361702127}];
+    var repCounts    = [{"name":"Jens Stoltenberg","party":"A","total":907,"count":18,"percent":1.9845644983461963},{"name":"Hans Olav Syversen","party":"KrF","total":664,"count":8,"percent":1.2048192771084338},{"name":"Jonas Gahr Støre","party":"A","total":809,"count":5,"percent":0.6180469715698393},{"name":"Knut Arild Hareide","party":"KrF","total":449,"count":5,"percent":1.1135857461024499},{"name":"Trine Skei Grande","party":"V","total":1165,"count":5,"percent":0.4291845493562232},{"name":"Per Olaf Lundteigen","party":"Sp","total":686,"count":4,"percent":0.5830903790087464},{"name":"Snorre Serigstad Valen","party":"SV","total":360,"count":3,"percent":0.8333333333333334},{"name":"Gunnar Gundersen","party":"H","total":526,"count":3,"percent":0.5703422053231939},{"name":"Ketil Solvik-Olsen","party":"FrP","total":990,"count":3,"percent":0.30303030303030304},{"name":"Knut Storberget","party":"A","total":794,"count":3,"percent":0.3778337531486146},{"name":"Kristin Halvorsen","party":"SV","total":940,"count":3,"percent":0.3191489361702127},{"name":"Dagfinn Høybråten","party":"KrF","total":360,"count":3,"percent":0.8333333333333334},{"name":"Svein Flåtten","party":"H","total":491,"count":3,"percent":0.6109979633401221},{"name":"Olemic Thommessen","party":"H","total":356,"count":3,"percent":0.8426966292134831},{"name":"Sigurd Hille","party":"H","total":11,"count":3,"percent":27.27272727272727},{"name":"Karin Andersen","party":"SV","total":674,"count":2,"percent":0.2967359050445104},{"name":"Magnhild Meltveit Kleppa","party":"Sp","total":1086,"count":2,"percent":0.1841620626151013},{"name":"Abid Q. Raja","party":"V","total":257,"count":2,"percent":0.7782101167315175},{"name":"Torbjørn Røe Isaksen","party":"H","total":613,"count":2,"percent":0.3262642740619902},{"name":"Tord Lien","party":"FrP","total":480,"count":2,"percent":0.4166666666666667},{"name":"Torgeir Micaelsen","party":"A","total":200,"count":2,"percent":1.0},{"name":"Trygve Slagsvold Vedum","party":"Sp","total":460,"count":2,"percent":0.43478260869565216},{"name":"Kenneth Svendsen","party":"FrP","total":115,"count":2,"percent":1.7391304347826086},{"name":"Per Arne Olsen","party":"FrP","total":168,"count":1,"percent":0.5952380952380952},{"name":"Anniken Huitfeldt","party":"A","total":563,"count":1,"percent":0.17761989342806395}];
     var partyCounts  = [{"party":"A","total":16963,"count":43,"percent":0.25349289630371985,"name":"A"},{"party":"KrF","total":5637,"count":18,"percent":0.31931878658861096,"name":"KrF"},{"party":"H","total":12570,"count":16,"percent":0.12728719172633254,"name":"H"},{"party":"FrP","total":12084,"count":15,"percent":0.12413108242303873,"name":"FrP"},{"party":"SV","total":7037,"count":11,"percent":0.15631661219269574,"name":"SV"},{"party":"Sp","total":7127,"count":9,"percent":0.1262803423600393,"name":"Sp"},{"party":"V","total":3595,"count":9,"percent":0.25034770514603616,"name":"V"}, {"party":"MDG","total":277,"count":0,"percent":0.0,"name":"MDG"}];
     var timeline     = [{"session":"2008-2009","count":15,"total":14603,"percent":0.10271861946175444},{"session":"2009-2010","count":28,"total":15522,"percent":0.1803891251127432},{"session":"2010-2011","count":28,"total":17559,"percent":0.15946238396264023},{"session":"2011-2012","count":40,"total":15100,"percent":0.26490066225165565},{"session":"2012-2013","count":13,"total":9231,"percent":0.14082981258801863},{"session":"2013-2014","count":33,"total":13462,"percent":0.24513445253305602}];
 
@@ -8,21 +8,22 @@
         format: function(d) { return d + '%'; }
     };
 
-    function drawTopList(selector, data, key) {
+    function drawTopList(selector, data, key, type) {
         var cols = [];
+        var type = type || 'bar';
 
         var tickFormatter = key === 'percent' ? percentFormat : undefined;
         var yTitle        = key === 'percent' ? 'Prosent' : 'Antall';
 
         cols.push(['x'].concat(data.map(function (d) { return d.name; })));
-        cols.push([].concat(data.map(function (d) { return d[key]; })));
+        cols.push([yTitle].concat(data.map(function (d) { return d[key]; })));
 
         var chart = c3.generate({
             bindto: selector,
             data: {
                 x: 'x',
                 columns: cols,
-                type: 'bar'
+                type: type
             },
             transition: { duration: 0 },
             axis: {
@@ -39,7 +40,7 @@
         var cols = [];
 
         cols.push(['x'].concat(data.map(function(d) { return d.session; })));
-        cols.push(['Sesjon'].concat(data.map(function(d) { return d.percent; })));
+        cols.push(['Prosent av alle innlegg'].concat(data.map(function(d) { return d.percent; })));
 
         var chart = c3.generate({
             bindto: selector,
@@ -73,9 +74,8 @@
         });
     }
 
-    drawTopList('.om-munterhet-representative-chart', repCounts.slice(0, 6), 'count');
+    drawTopList('.om-munterhet-representative-chart', repCounts.slice(0, 23), 'count', 'line');
     drawTopList('.om-munterhet-party-chart', partyCounts, 'count');
     drawTopList('.om-munterhet-party-percent-chart', sortBy('percent', partyCounts), 'percent');
-
     drawTimeline('.om-munterhet-timeline-chart', timeline);
 }());
