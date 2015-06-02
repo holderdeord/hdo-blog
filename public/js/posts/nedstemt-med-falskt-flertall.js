37f4368bd92b4443a59795bccc4d3630d29e231b
Jari Bakken <jari@holderdeord.no>
2015-06-02 23:25:40 +0200

    Add chart of rebel percentage

--- public/js/posts/nedstemt-med-falskt-flertall.js
+++ public/js/posts/nedstemt-med-falskt-flertall.js
@@ -19,12 +19,57 @@
             legend: { show: false },
             color: { pattern: opts.colorPattern }
         });
-    };
+    }
+
+    var data = d3.entries({
+      "FrP": {
+        "total": 224150,
+        "rebel": 609
+      },
+      "H": {
+        "total": 199752,
+        "rebel": 717
+      },
+      "A": {
+        "total": 362889,
+        "rebel": 477
+      },
+      "KrF": {
+        "total": 58580,
+        "rebel": 151
+      },
+      "Sp": {
+        "total": 63104,
+        "rebel": 126
+      },
+      "SV": {
+        "total": 59102,
+        "rebel": 101
+      },
+      "V": {
+        "total": 21054,
+        "rebel": 28
+      },
+      "MDG": {
+        "total": 1334
+      }
+    });
+
+    data.forEach(function(d) {
+        d.value.rebelPercent = d.value.rebel ? (d.value.rebel / d.value.total)*100 : 0;
+    });
+
+    data.sort(function(a, b) {
+        return d3.descending(a.value.rebelPercent, b.value.rebelPercent);
+    });
+
+
+
 
     chart({
         node: '#percent-rebels-per-party',
-        names: ['test1', 'test2', 'test3', 'test4', 'test5', 'test6'],
-        values: [5, 10, 15, 20, 25, 30],
+        names: data.map(function(d) { return d.key; }),
+        values: data.map(function(d) { return d.value.rebelPercent; }),
         label: 'Prosent utbrytere'
     });
 
